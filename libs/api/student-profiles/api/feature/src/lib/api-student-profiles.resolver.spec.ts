e1a66ecf91d8d3198800298fdd786cd98172d007
Xander <u20465026@tuks.co.za>
2022-04-16 12:58:00 +0200

    ✅ (api):Created skeleton for unit tests

--- libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.spec.ts
+++ libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.spec.ts
@@ -1,19 +1,44 @@
+import {
+  ApiStudentProfilesEntity,
+  ApiStudentProfilesInputEntity,
+} from '@graduates/api/student-profiles/api/shared/data-access';
 import { ApiStudentProfileService } from '@graduates/api/student-profiles/service/feature';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiStudentProfileResolver } from './api-student-profiles.resolver';
-import { CommandBus } from '@nestjs/cqrs';
 
 describe('ApiStudentProfileResolver', () => {
   let resolver: ApiStudentProfileResolver;
+  const studentInput = new ApiStudentProfilesInputEntity();
+
+  studentInput.studentNum = '20465026';
+  studentInput.phoneNum = '0007894568';
+  studentInput.tags = ['AI', 'Big Data'];
+
+  const MockStudentService = {
+    findById: jest.fn((id) => {
+      return {
+        studentNum: id,
+        firstName: 'Xander',
+        lastName: 'Coetzer',
+      };
+    }),
+    update: jest.fn((input) => {
+      if (input.studentNum != 'u20465026') return null;
+      return input;
+    }),
+    delete: jest.fn((id) => {
+      if (id == 'u20465026') return 'Delete was successfull';
+      return 'Student does not exist';
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        ApiStudentProfileResolver,
-        ApiStudentProfileService,
-        CommandBus,
-      ],
-    }).compile();
+      providers: [ApiStudentProfileResolver, ApiStudentProfileService],
+    })
+      .overrideProvider(ApiStudentProfileService)
+      .useValue(MockStudentService)
+      .compile();
 
     resolver = module.get<ApiStudentProfileResolver>(ApiStudentProfileResolver);
   });
@@ -22,11 +47,29 @@ describe('ApiStudentProfileResolver', () => {
     expect(resolver).toBeDefined();
   });
 
-  describe('when the getStudent method is called ', () => {
-    describe('and the student exists', () => {
+  describe('@getStudent', () => {
+    describe('the student exists', () => {
       it('should return the student object', () => {});
     });
-    describe('and the student does not exist', () => {
+    describe('the student does not exist', () => {
+      it('should ', () => {});
+    });
+  });
+
+  describe('@editStudent', () => {
+    describe('the student exists', () => {
+      it('should return the student object', () => {});
+    });
+    describe('the student does not exist', () => {
+      it('should ', () => {});
+    });
+  });
+
+  describe('@deleteStudent', () => {
+    describe('the student exists', () => {
+      it('should return the student object', () => {});
+    });
+    describe('the student does not exist', () => {
       it('should ', () => {});
     });
   });
