da2051ec9cac701c9cbf13082a21a4fecc9ff1ed
Xander <u20465026@tuks.co.za>
2022-04-16 15:56:40 +0200

    ✅ (api):Added test for getTest

--- libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.spec.ts
+++ libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.spec.ts
@@ -9,6 +9,28 @@ import { ApiStudentProfileResolver } from './api-student-profiles.resolver';
 describe('ApiStudentProfileResolver', () => {
   let resolver: ApiStudentProfileResolver;
   const studentInput = new ApiStudentProfilesInputEntity();
+  const output = new ApiStudentProfilesEntity();
+  output.dateOfBirth = '19/09/1999';
+    output.phoneNum = '0834521355';
+    output.email = 'John.Wick@gmail.com';
+    output.firstName = 'John';
+    output.studentNum = 'u20465026';
+    output.lastName = 'Wick';
+    output.title = 'MSc';
+    output.nameOfDegree = 'Computer Science';
+    output.bio =
+      'From a young age John has showed promise, but it was not until age 20 that he got his second MSc...';
+    output.tags = ['Security','Hacking','Error elimination'];
+    output.employmentStatus = 'Unemployed, open to offers';
+    output.preferredLocation = 'Pretoria';
+    output.notableAchievements = ['Part of Facebook','Part of Goldenkey'];
+    output.links = [
+      ['discord', 'http'],
+      ['twitch', 'https'],
+    ];
+    output.academicRecord = false;
+    output.cv = true;
+    output.capstoneProject = true;
 
   studentInput.studentNum = '20465026';
   studentInput.phoneNum = '0007894568';
@@ -16,11 +38,12 @@ describe('ApiStudentProfileResolver', () => {
 
   const MockStudentService = {
     findById: jest.fn((id) => {
-      return {
-        studentNum: id,
-        firstName: 'Xander',
-        lastName: 'Coetzer',
-      };
+      return ['Xander',id,'Coetzer'];
+      // return {
+      //   studentNum: id,
+      //   firstName: 'Xander',
+      //   lastName: 'Coetzer',
+      // };
     }),
     update: jest.fn((input) => {
       if (input.studentNum != 'u20465026') return null;
@@ -48,19 +71,27 @@ describe('ApiStudentProfileResolver', () => {
   });
 
   describe('@getStudent', () => {
-    describe('the student exists', () => {
-      it('should return the student object', () => {});
+    describe('if the student exists', () => {
+      it('should return the student object', async () => {
+        const student = await (resolver.getStudent('u20465026'));
+        expect(student).toEqual(output);
+        expect(MockStudentService.findById).toBeCalled();
+      });
     });
-    describe('the student does not exist', () => {
+    describe('if the student does not exist', () => {
       it('should ', () => {});
     });
   });
 
   describe('@editStudent', () => {
-    describe('the student exists', () => {
-      it('should return the student object', () => {});
+    describe('if the student exists', () => {
+      it('should return the student object', async () => {
+        const student = await (resolver.editStudent(studentInput));
+        expect(student).toEqual(studentInput);
+        expect(MockStudentService.update).toBeCalled();
+      });
     });
-    describe('the student does not exist', () => {
+    describe('if the student does not exist', () => {
       it('should ', () => {});
     });
   });
